d113c8d7248d876e08c672045fa40e2ce64eb39a
Fulgence Degboe <defuko2000@yahoo.fr>
2021-08-19 21:26:17 +0200

    TerraformBuilder - First page

--- TerraformBuilderApp/urls.py
+++ TerraformBuilderApp/urls.py
@@ -6,5 +6,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('login/', views.log_in, name='login'),
+    path('logout/', views.log_out, name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- TerraformBuilderApp/views.py
+++ TerraformBuilderApp/views.py
@@ -1,6 +1,52 @@
-#from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-def index(request):
-    message = "Bonjour tout le monde"
-    return HttpResponse(message)
+from .forms import CreateUserForm, LoginForm
+
+
+def home(request):
+    if request.user.is_authenticated:
+        context = {}
+        return render(request, 'TerraformBuilderApp/dashboard.html', context)
+    else:
+        return redirect('login')
+
+
+def log_in(request):
+    if not request.user.is_authenticated:
+        form = LoginForm()
+        context = {'form': form}
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            else:
+                messages.error(request, "Username or password is incorrect !")
+                return render(request, 'TerraformBuilderApp/login.html', context)
+        else:
+            return render(request, 'TerraformBuilderApp/login.html', context)
+    else:
+        return redirect('/')
+
+
+def log_out(request):
+    logout(request)
+    return redirect('login')
+
+
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    context = {'form': form}
+    return render(request, 'TerraformBuilderApp/register.html', context)
